d06b71f14c0d8a3af2064c63c6915b881922f88c
basti_env <a@b.de>
2018-07-20 14:33:38 +0000

    web ready, admin fixed

--- form/admin.py
+++ form/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 
 from .models import Eintrag
 
-admin.site.register(Eintrag)
+class EintragAdmin(admin.ModelAdmin):
+    list_display = ('Name','Anmeldung', 'Essen','Email','pub_date')
+admin.site.register(Eintrag, EintragAdmin)
